908414dc8714b7a4e7dc91461eb83dd949154899
ksoftdev <kuntursoftdev@gmail.com>
2021-11-23 22:46:47 -0500

    02. Basic functionality

---
+++ src/main/java/com/misiontic/account_ms/exceptions/AccountNotFoundException.java
@@ -0,0 +1,6 @@
+package com.misiontic.account_ms.exceptions;
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/misiontic/account_ms/exceptions/InsufficientBalanceException.java
@@ -0,0 +1,6 @@
+package com.misiontic.account_ms.exceptions;
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
